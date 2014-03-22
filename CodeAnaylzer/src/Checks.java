3161e734af9540aa0b6a6832db4795ec6049d55c
benste <Benedict.Stein+git@gmail.com>
2014-03-23 00:01:44 +0100

    added Primitive Datatype Check

--- CodeAnaylzer/src/Checks.java
+++ CodeAnaylzer/src/Checks.java
@@ -3,15 +3,28 @@
  * @author steinb
  *
  */
-public class Checks {
+public class Checks{
 	/**
 	 * Checks if the text starts with a Java Access modifier
 	 * @param text
 	 * @return
 	 */
 	public boolean startswithACL(String text){
-		false;
-		
+		return false;
+	}
+	
+	public boolean startswithPrimitiveDatatype(String text){
+		boolean result = false ;
+		result = result || text.startsWith("boolean");
+		result = result || text.startsWith("byte");
+		result = result || text.startsWith("char");
+		result = result || text.startsWith("int");
+		result = result || text.startsWith("long");
+		result = result || text.startsWith("boolean");
+		result = result || text.startsWith("float");
+		result = result || text.startsWith("double");
+		result = result || text.startsWith("void");
+		return result
 	}
 	
 
