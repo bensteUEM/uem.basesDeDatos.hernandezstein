a99c83df29a42510af48348cbb1da3471603cacd
David <David@David-PC>
2014-03-23 18:57:42 +0100

    Purpose: TextSplitter.curlyCountLeveled() method testing

---
+++ CodeAnaylzer/TestFiles/03d - SmallSampleCOMMENT.java
@@ -0,0 +1,21 @@
+/**
+ * This class is used for testing the 
+ * TextSplitter.curlyCountLeveled() method
+ * @author davidh
+ *
+ */
+public class SmallSample {
+	
+	String s = "{{{{";
+	
+	/*
+	 * {{{{{
+	 * }}
+	 */
+	
+	//{{{{{{{{
+	
+	/**
+	 * {{{{{{
+	 */
+}
