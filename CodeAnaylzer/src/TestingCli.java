4b7cd0b83dca0ea82f174679f9402a63bcec7d83
David <David@David-PC>
2014-03-24 00:50:04 +0100

    More test cases included in the file

--- CodeAnaylzer/src/TestingCli.java
+++ CodeAnaylzer/src/TestingCli.java
@@ -11,23 +11,22 @@ public class TestingCli {
 		/*Gui gui = new Gui();
 		gui.setVisible(true);*/
 		
-		System.out.println(curlyBraceBalance());
+		System.out.println(deleteComments());
 		//String sss = "''";
+		//char c2 = '"';
 		
 	}
 	
-	public static boolean curlyBraceBalance(){
+	public static String deleteComments(){
 		// Opens & reads the file
 		TextSplitter ts = new TextSplitter("C:/Users/David/Desktop/MainCl2.txt");
 		ts.openFile();
 		String content = ts.readFile();
 		
 		// Needed variables
-		int level = 0;
 		boolean insideLineComment = false;
 		boolean insideMultilineComment = false;
-		boolean insideQuotes = false;
-		boolean insideSQuotes = false;
+		StringBuilder noComments = new StringBuilder();
 		
 		// Code processing
 		for(int i=0; i<content.length(); i++){ // Detects '/**' comment type
@@ -53,50 +52,20 @@ public class TestingCli {
 				insideLineComment = false;
 			}
 			
-			if(content.charAt(i) == '"' && insideLineComment==false && insideMultilineComment==false){
-				if(insideQuotes == false){
-					System.out.println("Inside Quotes");
-					insideQuotes = true;
-				} else {
-					System.out.println("Outside Quotes");
-					insideQuotes = false;
-				}
-			}
-			
-			if(String.valueOf(content.charAt(i)).equals("'") && insideLineComment==false 
-					&& insideMultilineComment==false && insideQuotes==false){
-				if(insideSQuotes == false){
-					System.out.println("Inside Simple Quotes");
-					insideSQuotes = true;
-				} else {
-					System.out.println("Outside Simple Quotes");
-					insideSQuotes = false;
-				}
-			}
-			
-			if(String.valueOf(content.charAt(i)).equals("*") && String.valueOf(content.charAt(i+1)).equals("/") ){
+			if(i>1 && String.valueOf(content.charAt(i-2)).equals("*") && String.valueOf(content.charAt(i-1)).equals("/") ){
 				System.out.println("End of comment");
 				insideMultilineComment = false;
 			}
 			
-			if(content.charAt(i) == '{' && insideLineComment==false && insideMultilineComment==false  
-					&& insideQuotes==false && insideSQuotes==false){
-				level++;
-			}
-			if(content.charAt(i) == '}' && insideLineComment==false && insideMultilineComment==false 
-					&& insideQuotes==false && insideSQuotes==false){
-				level--;
+			if(insideLineComment==false && insideMultilineComment==false){
+				noComments.append(content.charAt(i));
 			}
 			
 		}
-		
-		if(level == 0){
-			ts.closeFile();
-			return true;
-		} else {
-			ts.closeFile();
-			return false;
-		}
+
+		ts.closeFile();
+		return noComments.toString();
+
 	}
 
 }
