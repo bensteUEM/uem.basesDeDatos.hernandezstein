0f1b4cbab27d75363e20024c6bf3db4322b140d8
bensteUEM <Benedict.Stein+github@gmail.com>
2014-03-26 09:53:26 +0100

    Java Compiler Experiments

---
+++ CodeAnaylzer/src/CodeAnalyzerProcessor.java
@@ -0,0 +1,25 @@
+import java.util.Set;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.TypeElement;
+
+// This is an experimental code following the tutorial of
+// https://today.java.net/pub/a/today/2008/04/10/source-code-analysis-using-java-6-compiler-apis.html#accessing-the-abstract-syntax-tree-the-compiler-tree-api
+
+@SupportedSourceVersion(SourceVersion.RELEASE_7)
+//@SupportedAnnotationTypes("*")
+public class CodeAnalyzerProcessor extends AbstractProcessor {
+	@Override
+	public boolean process(Set<? extends TypeElement> annotations,
+			RoundEnvironment roundEnvironment) {
+		for (Element e : roundEnvironment.getRootElements()) {
+			System.out.println("Element is " + e.getSimpleName());
+			// Add code here to analyze each root element
+		}
+		return true;
+	}
+}
--- CodeAnaylzer/src/TestingCli.java
+++ CodeAnaylzer/src/TestingCli.java
@@ -1,4 +1,13 @@
 import java.io.File;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.tools.JavaCompiler;
+import javax.tools.JavaCompiler.CompilationTask;
+import javax.tools.JavaFileObject;
+import javax.tools.StandardJavaFileManager;
+import javax.tools.ToolProvider;
 
 public class TestingCli {
 
@@ -11,18 +20,50 @@ public class TestingCli {
 		// TODO Auto-generated method stub
 		// CodeAnalyzer program = new CodeAnalyzer();
 
-		/* GUI testing part
-		Gui gui = new Gui();
-		gui.setVisible(true);
-		*/
+		/*
+		 * GUI testing part Gui gui = new Gui(); gui.setVisible(true);
+		
 
-		/* TextSplitter Testing Part */
-		String path1 = "TestFiles"+File.separator+"01 - SmallSample.java";
-		String path2 = "TestFiles"+File.separator+"03d - SmallSampleCOMMENT.java";
+		/* TextSplitter Testing Part 
+		
+		String path2 = "TestFiles" + File.separator
+				+ "03d - SmallSampleCOMMENT.java";
 		TextSplitter test = new TextSplitter(path2);
-		//test.structureCode();
+		// test.structureCode();
 		System.out.println(test.curlyCountLeveled());
-		/* TextSplitter Testing Part END*/
+		 TextSplitter Testing Part END 
+		 */
+		String path1 = "TestFiles" + File.separator + "01 - SmallSample.java";
+		ArrayList<String> paths = new ArrayList(0);
+		paths.add(path1);
+		
+		// Code Anaylzer Process ...
+		//Get an instance of java compiler
+		JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+
+		//Get a new instance of the standard file manager implementation
+		StandardJavaFileManager fileManager = compiler.
+		        getStandardFileManager(null, null, null);
+		        
+		// Get the list of java file objects, in this case we have only 
+		// one file, TestClass.java
+		Iterable<? extends JavaFileObject> compilationUnits1 = 
+		        fileManager.getJavaFileObjectsFromStrings(paths);
+		
+		CompilationTask task = compiler.getTask(null, fileManager, null, null,
+				null, compilationUnits1);
+
+		// Create a list to hold annotation processors
+		LinkedList<AbstractProcessor> processors = new LinkedList<AbstractProcessor>();
+
+		// Add an annotation processor to the list
+		processors.add(new CodeAnalyzerProcessor());
+
+		// Set the annotation processor to the compiler task
+		task.setProcessors(processors);
+
+		// Perform the compilation task.
+		task.call();
 
 	}
 
